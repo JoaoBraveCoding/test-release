57dca040d80c3b298e576114f9fcddbe86d81af6
Joao Marcal <jmarcal@redhat.com>
2024-03-06 16:54:57 +0000

    feat: adds juice to repo

---
+++ operator/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/go-uuid"
+)
+
+func main() {
+	
+	fmt.Println("New feature for 0.1.0")
+
+	uid, err := uuid.GenerateUUID()
+	if err != nil {
+		fmt.Println("Error generating UUID")
+	}
+	fmt.Println("This is not really the Loki Operator, just meant to look like it. Here's a UUID: ", uid)
+}
