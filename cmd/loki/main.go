e918b9afcb4906240e59e00dc3f1f1b693c523fb
Joao Marcal <jmarcal@redhat.com>
2024-03-11 14:57:24 +0000

    feat: should not be picked up

--- cmd/loki/main.go
+++ cmd/loki/main.go
@@ -10,7 +10,7 @@ func main() {
 
 	uid, err := uuid.GenerateUUID()
 	if err != nil {
-		fmt.Println("Error generating UUID")
+		fmt.Println("Error generating UUI")
 	}
 	fmt.Println("This is not really Loki, just meant to look like it. Here's a UUID: ", uid)
 }
