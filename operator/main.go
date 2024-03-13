79d2b933d0872e245c27b226aae7b1a8a467b76a
Joao Marcal <jmarcal@redhat.com>
2024-03-13 20:47:01 +0000

    feat(operator): pog

--- operator/main.go
+++ operator/main.go
@@ -31,6 +31,8 @@ func main() {
 	fmt.Println("New feature for 0.12.0")
 	fmt.Println("New feature for 0.14.0")
 	fmt.Println("New feature for 0.14.0")
+	fmt.Println("New feature for 0.14.0")
+	fmt.Println("New feature for 0.14.0")
 
 	uid, err := uuid.GenerateUUID()
 	if err != nil {
