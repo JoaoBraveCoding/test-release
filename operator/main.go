87ab48755178a1d1dcc9d1f3ad712820c91d6fc5
Joao Marcal <jmarcal@redhat.com>
2024-03-13 20:44:46 +0000

    deps: added dependencies

--- operator/main.go
+++ operator/main.go
@@ -30,6 +30,7 @@ func main() {
 	fmt.Println("New feature for 0.12.0")
 	fmt.Println("New feature for 0.12.0")
 	fmt.Println("New feature for 0.14.0")
+	fmt.Println("New feature for 0.14.0")
 
 	uid, err := uuid.GenerateUUID()
 	if err != nil {
