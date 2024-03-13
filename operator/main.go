2dc4dff884053e300f5241bd8882807ebbd9ea27
Joao Marcal <jmarcal@redhat.com>
2024-03-13 20:28:09 +0000

    refactor(operator): test

--- operator/main.go
+++ operator/main.go
@@ -28,6 +28,7 @@ func main() {
 	fmt.Println("New feature for 0.12.0")
 	fmt.Println("New feature for 0.12.0")
 	fmt.Println("New feature for 0.12.0")
+	fmt.Println("New feature for 0.12.0")
 
 	uid, err := uuid.GenerateUUID()
 	if err != nil {
