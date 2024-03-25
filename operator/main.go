dbed6baf45ff84b568e34e7406cf0f67facdc9d0
Joao Marcal <jmarcal@redhat.com>
2024-03-25 17:57:25 +0000

    made a patch

--- operator/main.go
+++ operator/main.go
@@ -18,6 +18,7 @@ func main() {
 	fmt.Println("New feature for 0.5.0")
 	fmt.Println("New feature for 0.6.0")
 	fmt.Println("New feature for 0.7.0")
+	fmt.Println("New feature for 0.7.1")
 	
 	uid, err := uuid.GenerateUUID()
 	if err != nil {
