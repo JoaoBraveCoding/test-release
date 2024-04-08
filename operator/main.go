631f6da32a74509c284242ca8c205df52f7157b1
Joao Marcal <jmarcal@redhat.com>
2024-04-08 17:59:19 +0100

    feat(operator): test

--- operator/main.go
+++ operator/main.go
@@ -30,6 +30,7 @@ func main() {
 	fmt.Println("New feature for 0.10.3")
 	fmt.Println("New feature for 0.10.4")
 	fmt.Println("New feature for 0.10.2")
+	fmt.Println("New feature for 0.10.3")
 
 	
 	uid, err := uuid.GenerateUUID()
