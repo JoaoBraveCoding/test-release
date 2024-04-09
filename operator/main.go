4e3019babbe564de18d9f41aa71292ed0bdd54fd
Joao Marcal <jmarcal@redhat.com>
2024-04-09 12:08:35 +0100

    feat(operator): commit

--- operator/main.go
+++ operator/main.go
@@ -68,6 +68,7 @@ func main() {
 	fmt.Println("New feature for 0.11.25")
 	fmt.Println("New feature for 0.12.0")
 	fmt.Println("manifests")
+	fmt.Println("New feature for 0.12.1")
 
 	
 	uid, err := uuid.GenerateUUID()
