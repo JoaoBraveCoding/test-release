096e63d0ac5cf44c2291502f627b3795d5bec3d4
Joao Marcal <jmarcal@redhat.com>
2024-04-09 11:21:28 +0100

    feat(operator): look in master

--- operator/main.go
+++ operator/main.go
@@ -56,6 +56,7 @@ func main() {
 	fmt.Println("New feature for 0.11.13")
 	fmt.Println("New feature for 0.11.14")
 	fmt.Println("New feature for 0.11.15")
+	fmt.Println("New feature for 0.11.16")
 
 	
 	uid, err := uuid.GenerateUUID()
