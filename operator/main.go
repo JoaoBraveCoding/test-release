5cfa6da9a65d4031f3cb149029328fac4a40dcc9
Joao Marcal <jmarcal@redhat.com>
2024-04-09 11:28:43 +0100

    feat(operator): bump

--- operator/main.go
+++ operator/main.go
@@ -58,6 +58,7 @@ func main() {
 	fmt.Println("New feature for 0.11.15")
 	fmt.Println("New feature for 0.11.16")
 	fmt.Println("New feature for 0.11.17")
+	fmt.Println("New feature for 0.11.18")
 
 	
 	uid, err := uuid.GenerateUUID()
